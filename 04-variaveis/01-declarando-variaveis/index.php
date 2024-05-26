c82b4d6de67b5b6a759e916b132738609b8ed136
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-05-25 21:12:33 -0300

    aulas concluidas, foi somente uma atualizacao entre arquivos locais e remotos

---
+++ 04-variaveis/01-declarando-variaveis/index.php
@@ -0,0 +1,20 @@
+<?php
+
+  $teste = "Algum texto";
+
+  echo $teste;
+
+  $num = 10;
+
+  echo "<br";
+  echo $num;
+
+  $_nome = "Rafael";
+
+  echo "<br";
+  echo $_nome;
+
+  echo "<br";
+  
+  $velocidadeMaxima = 100;
+  $velocidade_minima = 100;
---
+++ 04-variaveis/04-var-de-var/index.php
@@ -0,0 +1,9 @@
+<?php
+
+  $x = "nome"; // Valor de nome;
+
+  echo "$x <br>";
+
+  $$x = "Rafael"; // var com o nome de x (nome), com valor de Rafael
+
+  echo "$nome <br>";
