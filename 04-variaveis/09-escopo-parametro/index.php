406df6921befbaf0d9a91ada6193a0b0602b79ad
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-05-28 14:47:21 -0300

    Aula sobre escopo dos parametros

---
+++ 04-variaveis/09-escopo-parametro/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+    function soma($a, $b) {
+
+        echo $a + $b;
+        echo "<br><br>";
+
+    }
+
+    soma(2, -4);
+    echo "<br><br>";
+
+    soma(10, 5);
+    echo "<br><br>";
+
+    soma(6, 10);
