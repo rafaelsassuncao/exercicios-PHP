fbaae71ee0734671ffb20615a4ca788753f9cdd1
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-05-31 11:50:39 -0300

    Exercicio 010 - concluido com exito

---
+++ 05-op-e-expressoes/02-exerc010/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    /* Regras do exercicio: 
+      - Criar um arquivo PHP;
+      - Criar uma operação que contenha: (-), (/) e (*)
+      - Armazene todos os valores em variaveis;
+      - Imprima resultado final na tela;  
+
+    */
+
+
+    $a = 10;
+    $r = 5;
+    $k = 9;
+    
+
+    $resultado = ($r - $k) / $a * $k;
+    
+    echo $resultado;
+    echo "<br>";
