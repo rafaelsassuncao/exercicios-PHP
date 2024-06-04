d4ad7f5ac7e1fcba54ca487a573f191fcd5dc812
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-04 11:46:26 -0300

    Aula 05 sobre operacoes aritmeticas concluida.

---
+++ 05-op-e-expressoes/05-operacoes-aritmeticas/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+    $soma = 5 + 2;
+    echo $soma . "<br>";
+
+    $subtracao = 10 - 2;
+    echo $subtracao . "<br>";
+
+    $multiplicacao = 2 * 199;
+    echo $multiplicacao . "<br>";
+
+    $divisao = 123 / 48;
+    echo $divisao . "<br>";
+
+    $op = 5 + (2 / 7) * 8;
+    echo $op . "<br>";
+
+
+
