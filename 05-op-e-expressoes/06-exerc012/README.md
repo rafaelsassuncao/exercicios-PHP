6cd0d1e8008b8bc89304515b9a33d09d8fd4435f
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-04 14:42:49 -0300

    exerc012 concluido exito.

---
+++ 05-op-e-expressoes/06-exerc012/README.md
@@ -0,0 +1,11 @@
+Instruções dadas em aula de como é para ser feito o exercicio: 
+
+- Crie um arquivo PHP;
+
+- Crie uma  operação com cada um dos operadores básicos;
+
+- Cada operação deve estar em uma variavel diferente;
+
+- Imprima cada uma das etapas;
+
+- Ex: soma -> multiplicação -> divisão -> subtração
--- 05-op-e-expressoes/06-exerc012/index.php
+++ 05-op-e-expressoes/06-exerc012/index.php
@@ -1,1 +1,28 @@
 <?php
+
+    $a = 3;
+    $b = 12;
+    $op1 = $a - $b;
+    echo $op1 . "<br>";
+
+    $c = 12.4;
+    $op2 = $op1 * $c;
+    echo $op2 . "<br>";
+
+    $d = 4.8;
+    $op3 = $op2 + $d;
+    echo $op3 . "<br>";
+
+    $e = 9.2;
+    $op4 = $op3 / $e;
+    echo $op4 . "<br>";
+
+
+
+
+    
+
+
+
+
+
