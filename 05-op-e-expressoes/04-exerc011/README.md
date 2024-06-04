6f05131ce8a329bfb3dbd57b2b72b0b590fb925f
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-04 11:49:29 -0300

    eu criei um README para o exercicio 011

---
+++ 05-op-e-expressoes/04-exerc011/README.md
@@ -0,0 +1,5 @@
+O que é para ser feito no Exercicio 011:
+
+    - Teste a expressão "5" * 12;
+    
+    - Utilize a função gettype() com o resultado como parametro para checar o tipo de resultado da expressão.
--- 05-op-e-expressoes/04-exerc011/index.php
+++ 05-op-e-expressoes/04-exerc011/index.php
@@ -1,13 +1,5 @@
 <?php
 
-    /* Exercicio 011
-
-    - Teste a expressão "5" * 12;
-    - Utilize a função gettype() com o resultado como parametro para checar o tipo de resultado da expressão.
-
-    */
-
-
     $resultado = "5" * 12;
 
     echo $resultado . "<br>";
