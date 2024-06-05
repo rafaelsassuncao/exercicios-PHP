56794551facfb417a5100141f6cfaa7d860a73d0
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-05 14:59:11 -0300

    Aula de operador de modulo concluida.

---
+++ 05-op-e-expressoes/07-op-modulo/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+    echo 12 % 2;
+    echo " modulo <br>";
+    echo 12 / 2;
+    echo " divisão <br>";
+
+    echo 13 % 2 . "<br>"; 
+    echo 13 % 5 . "<br>";
+
+    $a = 20;
+    $b = 4;
+    $c = 12;
+
+    echo $a % $b . "<br>";
+    echo $a % $b . "<br>";
