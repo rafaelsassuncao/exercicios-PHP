189344542e10b38b3872957401b182a3a48807c3
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-12 05:48:47 -0300

    Aula sobre operações de identicos concluida.

---
+++ 05-op-e-expressoes/15-op-identico-a/index.php
@@ -0,0 +1,18 @@
+<?php
+
+  if(5 == "5"){
+    echo "5 é 5 (1)<br>";
+  }
+  
+  // op de idêntico
+  if(5 === "5"){
+    echo "5 é 5 (2)<br>";
+  }
+
+  if(5 === 5){
+    echo "5 é 5 (3)<br>";
+  }
+
+  if(5 === "teste"){
+    echo "É igual <br>";
+  }
