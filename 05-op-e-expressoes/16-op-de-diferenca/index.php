6b9d0cf64025c05601167eff70e862af60488c54
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-12 06:03:50 -0300

    verifiquei um erro que o código apresentava nas linhas 18 e 22.

--- 05-op-e-expressoes/16-op-de-diferenca/index.php
+++ 05-op-e-expressoes/16-op-de-diferenca/index.php
@@ -15,10 +15,10 @@
     echo "Testando diferença (3)<br>";
   }
   
-  if($a != "3"){
+  if(3 != "3"){
     echo "Testando diferença (4)<br>";
   }
   
-  if($a != 3){
-    echo "Testando diferença (2)<br>";
+  if(3 != "4"){
+    echo "Testando diferença (5)<br>";
   }
