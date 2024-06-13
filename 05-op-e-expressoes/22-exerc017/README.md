82a6a78f2f2f374a1b5e608346eb508aa9e069e0
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-13 11:08:46 -0300

    Exercicio 17 concluido com exito.

---
+++ 05-op-e-expressoes/22-exerc017/README.md
@@ -0,0 +1,11 @@
+# Exercicio 17
+
+O que é para ser feito no desafio:
+
+Verifique as seguintes operações com AND;
+
+* 15 > 5 AND "João" === "João";
+
+* "teste" > 5 AND 1
+
+* 2 === 3 AND 5 >= 3
---
+++ 05-op-e-expressoes/22-exerc017/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Comparação 1
+    if(15 > 5 && "João" === "João"){
+        echo "A comparação 1 é verdadeira!<br>";
+    }
+
+    // Comparação 2
+    if("teste" > 5 && 1){
+        echo "A comparação 2 é verdadeira!<br>";
+    }
+
+    // Comparação 3
+    if(2 === 3 && 5 >= 3){
+        echo "A comparação 3 é verdadeira!s";
+    }
