4ede93b9a1aa86555d99a04c322ea48c7809c034
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-17 06:25:22 -0300

    Aula sobre operador NOT concluida.

---
+++ 05-op-e-expressoes/25-op-NOT/index.php
@@ -0,0 +1,16 @@
+<?php
+
+  if(!(5 > 2)){ //true virou false 
+    echo "A operação 1 é verdadeira <br>";
+  }
+
+  if(!(5 > 20)){ //false virou true
+    echo "A operação 2 é verdadeira <br>";
+  }
+
+  $a = 10;
+  $b = 20;
+
+  if(!($a >= $b)){
+    echo "A operação 3 é verdadeira";
+  }
