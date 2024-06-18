ecf470ffb51e14a7bb9ca40838f058411b83d567
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-18 07:12:20 -0300

    exercicio 20 concluido com exito.

---
+++ 05-op-e-expressoes/30-exerc020/README.md
@@ -0,0 +1,9 @@
+# Exercicio 20
+
+O que é para ser feito nesse desafio:
+
+-   Atribua dois números a variáveis distintas;
+
+-   Faça uma comparação de menor ou igual com o operador ternário;
+
+-   Imprima resultados para ambas as possibilidades;
---
+++ 05-op-e-expressoes/30-exerc020/index.php
@@ -0,0 +1,10 @@
+<?php
+
+  $a = 10;
+  $b = 50;
+
+  echo $a <= $b && $a == $b ? "Deu true" : "Deu false <br>";
+  
+  echo $a <= $b ? "A é menor ou igual a B <br>" : "B é menor que A <br>";
+
+  echo $a >= $b ? "A é menor ou igual a B <br>" : "B é maior ou igual ao A <br>";
