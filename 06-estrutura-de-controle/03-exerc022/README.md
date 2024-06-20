287ec02dc471b2104aad2777f123bbfa4ed34014
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-20 10:33:45 -0300

    Exercicio 22 concluido com exito.

---
+++ 06-estrutura-de-controle/03-exerc022/README.md
@@ -0,0 +1,7 @@
+# Exercicio 22
+
+- Crie variáveis que recebem idade;
+
+- Cheque se as idades são maiores ou iguais a 18;
+
+- Se sim, imprima uma mensagem que a pessoa é maior de idade; 
---
+++ 06-estrutura-de-controle/03-exerc022/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $idade1 = 33;
+    $idade2 = 16;
+    $idade3 = 18;
+
+    $maiorIdade = 18;
+    
+    if($idade1 >= $maiorIdade){  
+        echo "Idade1 é maior de 18 anos! <br>"; 
+    } 
+
+    if($idade2 >= $maiorIdade){  
+        echo "Idade2 é maior de 18 anos! <br>"; 
+    } 
+
+    if($idade3 >= $maiorIdade){  
+        echo "Idade3 é maior de 18 anos! <br>"; 
+    } 
