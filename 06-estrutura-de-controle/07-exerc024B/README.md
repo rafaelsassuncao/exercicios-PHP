054fd745ecd24e3e53616c16e51ebb9c8be5ead7
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-20 14:31:58 -0300

    Exercicio 24B desafio concluido com exito.

---
+++ 06-estrutura-de-controle/07-exerc024B/README.md
@@ -0,0 +1,8 @@
+# Exercicio 24B
+
+- Crie uma variável que recebe um peso;
+  
+- Caso seja maior que 80, imprima a mensagem que está pesado demais;
+  
+- Se não, imprima "Peso dentro do limite;
+
---
+++ 06-estrutura-de-controle/07-exerc024B/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$peso = 80;
+
+$pesoLimite = 80;
+
+$msg1 = "Você esta acima do peso";
+$msg2 = "Peso está dentro do limite";
+
+if($peso > $pesoLimite){
+
+    echo $msg1;
+
+}else {
+
+    echo $msg2;
+
+}
