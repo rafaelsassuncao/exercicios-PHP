99706d74e78871504272bab2f5d4c38ff9ed9f3e
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-20 12:51:14 -0300

    Exercicio 24A concluido

--- 06-estrutura-de-controle/06-exerc024/index.php
+++ 06-estrutura-de-controle/06-exerc024/index.php
@@ -1,3 +1,35 @@
 <?php
 
-    
+    $item1 = "PHP";
+    $item2 = 10;
+    $item3 = true;
+
+    $msg1 = "Esta variável é um inteiro! <br>";
+    $msg2 = "Esta variável não é um inteiro! <br>";
+
+    // Verificação do item1
+    if(is_int($item1)){
+        
+        echo $msg1;
+
+    }else {
+        echo $msg2;
+    }
+
+    // Verificação do item2
+    if(is_int($item2)){
+        
+        echo $msg1;
+
+    }else {
+        echo $msg2;
+    }
+
+    // Verificação do item3
+    if(is_int($item3)){
+        
+        echo $msg1;
+
+    }else {
+        echo $msg2;
+    }
