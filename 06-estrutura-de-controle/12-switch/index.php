10c80adb8977b3783989a8ed43fcbf7e1598066e
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-26 06:16:57 -0300

    Aula sobre switch concluida.

---
+++ 06-estrutura-de-controle/12-switch/index.php
@@ -0,0 +1,39 @@
+<?php
+
+  $x = 10;
+
+  switch($x){
+
+    case 0: 
+      echo "X é igual a 0 <br>";
+      break;
+
+    case 1: 
+      echo "X é igual a 1 <br>";
+      break;  
+
+      default:
+        echo "X não é igual a nenhum dos números! <br>";
+
+  }
+
+
+  // Exemplo dado pelo professor para quando for variaveis com nome de pessoas:
+
+$y = "";
+
+switch($y) {
+
+  case "Matheus":
+    echo "O nome é Matheus! <br>";
+    break;
+
+  case "João":
+    echo "O nome é João! <br>";
+    break;
+
+  default:
+    echo "O nome não foi encontrado!"; 
+
+  }
+
