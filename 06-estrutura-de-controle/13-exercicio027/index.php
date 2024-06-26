642b18b7c710b7790b43fa6d734c28bf2dad12a3
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-26 08:13:16 -0300

    ainda não conclui o exercicio 027

---
+++ 06-estrutura-de-controle/13-exercicio027/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$primeiroValor = 5;
+$segundoValor = 10;
+
+  function compararNumeros($primeiroValor, $segundoValor){
+
+    echo $primeiroValor >= $segundoValor
+    echo "<br><br>";
+    
+    echo $segundoValor > $primeiroValor
+
+      echo "O segundo número é maior";
+
+    } else{
+
+      echo "Os números são iguais!";
+
+    }
+
+  }
