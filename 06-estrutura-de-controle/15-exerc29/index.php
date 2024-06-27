d4eaf517f8c5640424372d21e80c62723202254f
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-27 06:54:20 -0300

    conclui o desafio 29, mas mesmo rodando corretamente aqui no vs code, o mesmo não esta sendo aprovado no editor interno da Udemy.

--- 06-estrutura-de-controle/15-exerc29/index.php
+++ 06-estrutura-de-controle/15-exerc29/index.php
@@ -2,73 +2,31 @@
 
 function calcularDesconto($valorProduto, $categoria) {
 
-    $desconto = 0;
-    $descontoEletro = "eletronicos";
-    $descontoVestuario = "vestuario";
-    $descontoAlimentos = "alimentos";
+    if($categoria === "eletronicos"){
+        $desconto = 0.1;
+    
+    } elseif ($categoria === "vestuario"){
+        $desconto = 0.2;
 
-    switch ($categoria) {
-        case "eletronicos":
-            $desconto = 00.1;  // 10% de desconto
-            break;
-        case "vestuario":
-            $desconto = 00.2;  // 20% de desconto
-            break;
-        case "alimentos":
-            $desconto = 0.05;  // 5% de desconto
-            break;
-        }
-        
+    } elseif ($categoria === "alimentos"){
+        $desconto = 0.05;
+   
+    } else {
+        $desconto = 0;
+    }  
 
+$valorComDesconto = $valorProduto - ($valorProduto * $desconto);
 
-
+return $valorComDesconto;
 
 }
 
+$produto1 = calcularDesconto(100,"eletronicos");
+$produto2 = calcularDesconto(50,"vestuario");
+$produto3 = calcularDesconto(30,"alimentos");
+$produto4 = calcularDesconto(200,"brinquedos");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     $desconto = 0;  // Desconto sem categoria
-
-//   switch ($categoria) {
-//     case "eletronicos":
-//       $desconto = 10.0;  // 10% de desconto
-//       break;
-//     case "vestuario":
-//       $desconto = 20.0;  // 20% de desconto
-//       break;
-//     case "alimentos":
-//       $desconto = 5.0;  // 5% de desconto
-//       break;
-//   }
-
-//   $valorDesconto = $valorProduto * $desconto;
-//   $valorFinal = $valorProduto - $valorDesconto;
-
-//   return $valorFinal;
-// }
-
-// $valorProduto = 10;
-// $categoria = "alimentos";
-
-// $valorComDesconto = calcularDesconto($valorProduto, $categoria);
-// echo "Valor original: R$ " .$valorProduto, 2;
-// echo "<br> Valor com desconto ($categoria): R$ " . $valorComDesconto, 2;
+echo "Produto1 custa com desconto: R$" . $produto1;
+echo "<br>Produto2 custa com desconto: R$" . $produto2;
+echo "<br>Produto3 custa com desconto: R$" . $produto3;
+echo "<br>Produto4 custa com desconto: R$" . $produto4;
