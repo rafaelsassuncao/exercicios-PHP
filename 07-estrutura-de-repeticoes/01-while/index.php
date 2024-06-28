6aa44a8154fb8f41077744dff512113c0ade281a
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-06-28 15:06:07 -0300

    Aula sobre while concluida

---
+++ 07-estrutura-de-repeticoes/01-while/index.php
@@ -0,0 +1,67 @@
+<?php
+
+    // A definição do contador 
+    $x = 0;
+
+    // Inicio / Definição estrutura
+    while($x < 10){
+        
+        // Corpo do loop
+        echo $x . "<br>";
+
+        // Incremento do contador
+        $x++;
+
+    }
+
+    echo "Continuando o código";
+
+        // A definição do contador 
+        $y = 0;
+
+        // Inicio / Definição estrutura
+        while($y < 10){
+            
+            // Corpo do loop
+            echo $y . "<br>";
+    
+            // Incremento do contador
+            $y += 2;
+    
+        }
+
+    
+    echo "Continuando o código";
+
+
+        // A definição do contador 
+        $z = 10;
+
+        // Inicio / Definição estrutura
+        while($z > 0){
+            
+            // Corpo do loop
+            echo $z . "<br>";
+    
+            // Incremento do contador
+            $z--;
+    
+        }
+
+    echo "Continuando o código";
+        
+   // A definição do contador 
+   $a = 10;
+
+   // Inicio / Definição estrutura
+   while($a > 0){
+       
+       // Corpo do loop
+       if($a % 2 != 0){
+        echo $a . "<br>";
+       }
+
+       // Incremento do contador
+       $a--;
+
+   }
