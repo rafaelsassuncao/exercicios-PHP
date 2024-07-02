f8977eafae56b072736b09e13623e58e90b0ab89
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-07-02 10:20:53 -0300

    Tive um pouco de dificuldade para concluir este desafio 29, desafio como usar o "continue".

--- 07-estrutura-de-repeticoes/06-exerc029-continue/index.php
+++ 07-estrutura-de-repeticoes/06-exerc029-continue/index.php
@@ -2,16 +2,23 @@
 
   $algunsNumeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-  $y = 0;
+  $i = 0;
 
-  while($y < count($algunsNumeros)){
+  while($i < count($algunsNumeros)){
 
-    echo " Elemento: $algunsNumeros[$y] <br>";
+    $numeroAtual = $algunsNumeros[$i];
 
-  $y++;
+    if($numeroAtual == 30 || $numeroAtual == 40){
+
+      $i++;
+      continue;
+
+    }
+
+    echo " Elemento: $numeroAtual <br>";
+
+  $i++;
 
   }
-
-  if
 
   
