5e0e79e03dd08c15a3b04c6bf86a6a73798f2bed
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-07-02 07:12:25 -0300

    falta concluir o exercicio 29

---
+++ 07-estrutura-de-repeticoes/06-exerc029-continue/index.php
@@ -0,0 +1,17 @@
+<?php
+
+  $algunsNumeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+  $y = 0;
+
+  while($y < count($algunsNumeros)){
+
+    echo " Elemento: $algunsNumeros[$y] <br>";
+
+  $y++;
+
+  }
+
+  if
+
+  
