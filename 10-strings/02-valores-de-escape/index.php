f05de45b040ce9158b65ad1a4bc854e749cb8fab
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-07-11 10:43:23 -0300

    Aula sobre valores de escape concluida.

---
+++ 10-strings/02-valores-de-escape/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    header("Content-type: text/plain");
+
+    // pular linha
+    echo "Isso aqui vai ficar na primeira linha \n E isso na segunda linha \n";
+
+    //tab
+    echo "Testando o tab \t aqui \n";
+
+    //Barra Invertida 
+    echo "Barra invertida \\ \n";
+
+     //Dólar
+     echo "Imprimindo o dólar \$ teste";
