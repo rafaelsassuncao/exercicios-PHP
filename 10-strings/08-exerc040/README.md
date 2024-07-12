9d809366e36bc89a82a7871fa1ebd353b922b66a
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-07-12 06:23:52 -0300

    Parei o exercicio 40 na metade, vou continuar no meu serviço.

---
+++ 10-strings/08-exerc040/README.md
@@ -0,0 +1,5 @@
+# Exercicio 40
+
+-   Percorra a string: O rato roeu a roupa do rei de Roma, a partir de um loop;
+
+-   Imprima o número de letras "a" desta string;
---
+++ 10-strings/08-exerc040/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  $frase = "O rato roeu a roupa do rei de Roma";
+
+function selecionarCaracter($umaLetra){
+
+  $letraSelecionada = 0;
+
+  for($i = 0; $i < strlen($umaLetra);$i++){
+
+  if($umaLetra[$i])
+
+  }
+
+}  
