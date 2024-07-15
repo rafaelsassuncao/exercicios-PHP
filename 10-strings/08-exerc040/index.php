ea642f309b2145beea1e0fa41d97e07e276174d2
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-07-15 05:39:29 -0300

    Acrescentei alguns comentarios no codigo do exercicio 40.

--- 10-strings/08-exerc040/index.php
+++ 10-strings/08-exerc040/index.php
@@ -1,15 +1,21 @@
 <?php
 
+  // Esta é a frase onde vamos contar a letra 'a'
   $frase = "O rato roeu a roupa do rei de Roma";
 
+  // Esta variável vai contar quantas vezes a letra 'a' aparece
   $letraSelecionada = 0;
 
-  for($i = 0; $i < strlen($frase);$i++){
-
+  // Usamos um loop para passar por cada letra da frase
+  for($i = 0; $i < strlen($frase); $i++){
+    
+    // Se a letra na posição $i for 'a', incrementamos a contagem
     if($frase[$i] === "a"){
       $letraSelecionada++;
     }
     
   }  
 
+  // Exibimos o número de vezes que a letra 'a' aparece na frase
   echo "O numero de 'As' na frase é de : $letraSelecionada";
+?>
