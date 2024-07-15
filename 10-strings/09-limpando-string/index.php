318ff17a6b316be947f1d716e36954333118f12b
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-07-15 05:22:07 -0300

    Aula sobre trim e rtrim concluida.

---
+++ 10-strings/09-limpando-string/index.php
@@ -0,0 +1,14 @@
+<?php
+
+  $str1 = "   Rafael   ";
+
+  echo "Esta é a string 1: $str1. <br>";
+
+  $str1Limpa = trim($str1);
+
+  echo "Esta é a string 1 Limpa: $str1Limpa. <br>";
+
+
+  $str1Limpa2 = rtrim($str1);
+
+  echo "Esta é a string 2 Limpa: $str1Limpa2. <br>";
