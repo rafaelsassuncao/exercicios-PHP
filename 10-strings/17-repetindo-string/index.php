9b3148e23fca123777de8c1c6b10ea29f5fefa54
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-07-23 05:42:54 -0300

    Aula como usar o str_repeat.

---
+++ 10-strings/17-repetindo-string/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// 1º Modo:
+// Definindo a string que será repetida
+$str = "Teste";
+
+// Utilizando a função str_repeat para repetir a string $str 5 vezes
+$strRepetida = str_repeat($str, 5);
+
+// Exibindo a string repetida seguida de uma quebra de linha (<br>)
+echo $strRepetida . "<br>";
+
+// 2º Modo
+// Definindo a frase que será repetida
+$frase = "Testanto repetição por frase";
+
+// Utilizando a função str_repeat para repetir a string $frase 5 vezes
+echo str_repeat($frase, 5);
+?>
