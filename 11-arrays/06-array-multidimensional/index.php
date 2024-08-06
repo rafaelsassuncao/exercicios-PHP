7fd168da7ae752eb5966dc4c595eed61d0371201
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-08-06 08:17:24 -0300

    Aula sobre array multidimencional

---
+++ 11-arrays/06-array-multidimensional/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  $arr = [
+
+    [1, 2, 3],
+    [2, 4, 6],
+
+  ];
+
+  print_r($arr);
+  echo "<br";
+
+  echo $arr = [0][1] . "<br>"; //acessando primeiro array, e segundo elemento
+
+  echo $arr = [1][2] . "<br>"; //acessando segundo array, e ultimo elemento
