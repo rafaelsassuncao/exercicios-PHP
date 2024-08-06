9a2b2d414522354da9ef7bcc602abe76b01759f9
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-08-06 05:38:24 -0300

    Para conclusão do exercicio 45 foi preciso utilizar o range, for, if e else. No README consta o que era pedido para conclusão do mesmo.

--- 11-arrays/04-exerc045/index.php
+++ 11-arrays/04-exerc045/index.php
@@ -4,9 +4,21 @@
 
   for($i = 0; $i < count($arr); $i++) {
   
-    if($arr[$i] )
+    $soma = $arr[$i] + 6;
+
+    if($soma > 30) {
+
+      echo "O número $soma é muito alto <br>";
+
+    } else {
+
+      echo "$soma <br>";
+
+    }
   
   }
+
+
 
 
 
