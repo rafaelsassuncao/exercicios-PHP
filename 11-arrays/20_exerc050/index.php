a3919c943755efd368a6fa582d42cbf19793c32a
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-08-23 13:08:02 -0300

    no exerc050 adicionei alguns comentarios ao longo do código e  melhorei o README.

--- 11-arrays/20_exerc050/index.php
+++ 11-arrays/20_exerc050/index.php
@@ -1,5 +1,6 @@
 <?php
 
+  // Criação de um array associativo onde os nomes são as chaves e as idades são os valores
   $pessoa = [
     'Rafael' => 33, 
     'Thais'=> 31,
@@ -8,15 +9,22 @@
 
 ?>
 
+<!-- Início da tabela HTML com bordas -->
 <table border="1">
+  <!-- Cabeçalho da tabela, definindo as colunas: "Nomes" e "Idades" -->
   <tr>
     <th>Nomes</th>
     <th>Idades</th>
   </tr>
+  
+  <!-- Loop que percorre o array $pessoa para preencher as linhas da tabela -->
   <?php foreach($pessoa as $nomes => $idade): ?>
     <tr>
+      <!-- Exibe o nome na primeira coluna -->
       <td><?= $nomes; ?></td>
+      <!-- Exibe a idade correspondente na segunda coluna -->
       <td><?= $idade; ?></td>
     </tr>
   <?php endforeach; ?>
 </table>
+s
