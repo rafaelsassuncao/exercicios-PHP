3517beca90079aeb6b59fd86b769ac1bb382d408
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-09-06 06:55:03 -0300

    Nao conclui ainda o exercicio 9

--- 11-arrays/31_exerc_programacao9/index.php
+++ 11-arrays/31_exerc_programacao9/index.php
@@ -1,3 +1,16 @@
 <?php
 
-  
+  $arr1 = range(1, 20);
+
+  function somaElementos($arr1){
+
+    for($i = 0, $i < count($arr1), $i++){
+      $soma = array_sum($arr1);
+    };
+    return $soma;
+  };
+
+  $resultado = $soma;
+
+  echo $resultado;
+  echo "<br>";
