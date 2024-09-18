57856529c22f8f2ce7a96aca2ae866407f435645
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-09-18 06:31:49 -0300

    exercicio 052 concluido com exito.

---
+++ 12-intro_OOP/003_exerc052/README.md
@@ -0,0 +1,5 @@
+# Exercicio 52
+
+-   Crie uma classe **Car**;
+
+-   Instancie 3 objetos com esta classe;
---
+++ 12-intro_OOP/003_exerc052/index.php
@@ -0,0 +1,10 @@
+<?php
+
+  class Car {
+
+
+  }
+
+  $peugeot = new Car;
+  $onix = new Car;
+  $sandero = new Car;
