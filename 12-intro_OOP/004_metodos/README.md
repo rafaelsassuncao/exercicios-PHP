2a79e3707c4f6a5ccec6a2be878779729e09a605
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-09-18 06:48:39 -0300

    Aula como declarar métodos, concluida.

---
+++ 12-intro_OOP/004_metodos/README.md
@@ -0,0 +1,7 @@
+# Declarando Métodos
+
+-   Para declarar um métodos vamos utilizar a **sintaxe da function**, porém dentro de uma **class**;
+
+-   O restante é exatamente igual a sintaxe de função;
+
+-   Podemos retornar ou imprimir dados, dependendo da nossa regra de negócios;
---
+++ 12-intro_OOP/004_metodos/index.php
@@ -0,0 +1,27 @@
+<?php
+
+  class Pessoa {
+
+    function falar(){
+      echo "Olá, eu sou um objeto! <br>";
+    }
+
+    function somar($x, $y){
+      echo $x + $y . "<br>";
+    }
+
+  }
+
+  $rafael = new Pessoa;
+  $rafael->falar();
+  $rafael->falar();
+
+  $thais = new Pessoa;
+
+  $thais->falar();
+
+  $rafael->somar(10, 10);
+  $thais->somar(2, 3);
+  
+
+  
