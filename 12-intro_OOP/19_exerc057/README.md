f0ea5a2fc0f54cdc901f5a087d403c398237409e
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-10-03 08:21:10 -0300

    Exercicio 57 iniciado, vou concluir o mesmo hoje no final do dia.

---
+++ 12-intro_OOP/19_exerc057/README.md
@@ -0,0 +1,7 @@
+# Exercicio 57
+
+-   Crie uma **classe Cachorro** com propriedades;
+
+-   Inicie as propriedades via **constructor**;
+
+-   Crie um método para exibir cada uma das propriedades que você criou;
---
+++ 12-intro_OOP/19_exerc057/index.php
@@ -0,0 +1,22 @@
+<?php
+
+  class Cachorro{
+    public $cor;
+    public $raca;
+    public $tamanho;
+
+    function __construct($cor, $raca, $tamanho){
+      $this->cor = $cor;
+      $this->raca = $raca;
+      $this->tamanho = $tamanho;
+    }
+    public function exibirAnimal(){
+      echo "O cachorro da raça $this->raca, nasceu na cor $this->cor e vai ser de porte $this->tamanho ! <br><br>";
+    }
+  }
+
+  $fila = new Cachorro("tigrado", "Fila Brasileiro", "grande");
+  
+
+  $viraLata = new Cachorro("caramelo", "vira lata", "médio");
+  
