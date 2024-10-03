a0180bef5a5fede92140e3009964e227784ca487
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-10-03 05:42:31 -0300

    Aula sobre como utilizar o abstract, concluida.

--- 12-intro_OOP/17_classses_abstratas/index.php
+++ 12-intro_OOP/17_classses_abstratas/index.php
@@ -1,8 +1,28 @@
 <?php
 
   abstract class Teste {
-    public static function testantoClasse() {
+
+  public static function testandoClasse() {
+
     echo "Este método é de uma classe abstrata <br>";
+
   }
-    abstract public function testeAbs();
+
+  abstract public function testeAbs();
+
   }
+
+ // $t = new Teste;
+
+  Teste::testandoClasse();
+
+  class Nova extends Teste {
+
+    public function testeAbs() {
+      echo "Teste método abstrato <br>";
+    }
+
+  }
+
+  $n = new Nova;
+  $n->testeAbs();
