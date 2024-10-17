4643341eb2c712050f4806baa565cca83b84146a
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2024-10-17 05:37:39 -0300

    Aula sobre como verificar um Objeto concluida.

---
+++ 12-intro_OOP/22_verificandoObjetos/README.md
@@ -0,0 +1,7 @@
+# Verificando Objetos
+
+-   **Em PHP temos alguns métodos** que nos ajudam a entender melhor os objetos;
+
+-   is_object() => Verifica se uma variável é um objeto;
+-   get_class() => Verifica a classe de um objeto;
+-   method_exists() => Verifica se um método existe em um objeto;
---
+++ 12-intro_OOP/22_verificandoObjetos/index.php
@@ -0,0 +1,36 @@
+<?php
+
+  class Humano {
+    public function falar(){
+      echo "Olá";
+    }
+  }
+
+  $matheus = new Humano;
+  $teste = 10;
+
+  if(is_object($matheus)){
+    echo "É um objeto <br>";
+  }else{
+    echo "Não é um objeto <br>";
+  }
+
+  if(is_object($teste)){
+    echo "É um objeto <br>";
+  }else{
+    echo "Não é um objeto <br>";
+  }
+
+  echo get_class($matheus) . "<br>";
+
+  if(method_exists($matheus, "falar")){
+    echo "É um método! <br>";
+  }else{
+    echo "Não é um método! <br>";
+  }
+
+  if(method_exists($matheus, "asd")){
+    echo "Método existe! <br>";
+  }else{
+    echo "Método não existe! <br>";
+  }
