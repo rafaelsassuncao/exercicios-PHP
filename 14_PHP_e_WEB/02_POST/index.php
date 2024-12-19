bd585ed1ef4911e8bf7a347c2297007551b2d13d
Rafael Assuncao <rafael_assuncao@outlook.com.br>
2024-12-19 15:06:52 -0300

    Editei da linha 12 a linha 14

--- 14_PHP_e_WEB/02_POST/index.php
+++ 14_PHP_e_WEB/02_POST/index.php
@@ -9,9 +9,9 @@
   <form action="cadastro.php" method= "POST">
     <div><input type="text" name="marca" placeholder="Marca do Carro"></div>
     <div><input type="text" name="preco" placeholder="Preço do Carro"></div>
-    <div><input type="checkbox" name="opcionais[]" value="Teto Solar"></div>
-    <div><input type="checkbox" name="opcionais[]" value="Pelicula"></div>
-    <div><input type="checkbox" name="opcionais[]" value="Blindado"></div>
+    <div><input type="checkbox" name="opcionais[]" value="Teto Solar">Teto Solar</div>
+    <div><input type="checkbox" name="opcionais[]" value="Pelicula">Pelicula</div>
+    <div><input type="checkbox" name="opcionais[]" value="Blindado">Blindado</div>
     <div><input type="submit" value="Enviar"></div>
   </form>
 </body>
