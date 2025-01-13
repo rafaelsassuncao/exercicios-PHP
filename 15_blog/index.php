4c4d3e6dbd16fec1ad8ed135ae010b45e1454aa1
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-01-13 18:21:01 -0300

    Fiz algumas ediçoes nos arquivos index, css e

--- 15_blog/index.php
+++ 15_blog/index.php
@@ -1,12 +1,23 @@
+<?php
+  include_once('helpers/url.php');
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Blog Codar</title>
-  <link rel="stylesheet" href="style.css">
-</head>
+  <!-- ESTILO DO PROJETO -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+  <!-- GOOGLE FONTES - FONTES DO PROJETOS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+  </head>
 <body>
+  <h1>Olá Mundo!</h1>
   
 </body>
 </html>
