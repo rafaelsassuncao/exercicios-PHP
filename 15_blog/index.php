b0aa829028a87587ac9ff3c67ac8a743d9c821f8
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-01-27 05:50:05 -0300

    Terminei de editar o arquivo index conforme o professor orientou em aula.

--- 15_blog/index.php
+++ 15_blog/index.php
@@ -1,5 +1,5 @@
 <?php
-  include_once("templates/header.php")
+  include_once("templates/header.php");
 ?>
   <main>
     <div id="title-container">
@@ -7,13 +7,22 @@
       <p>O seu blog de programação</p>
     </div>
     <div id="posts-container">
-        <?php foreach($posts as $posts): ?>
-          <div class="post=box">
-            <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="">
+      <?php foreach($posts as $post): ?>
+        <div class="post-box">
+          <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+          <h2 class="post-title">
+            <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+          </h2>
+          <p class="post-description"><?= $post['description'] ?></p>
+          <div class="tags-container">
+            <?php foreach($post['tags'] as $tag): ?>
+              <a href="#"><?= $tag ?></a>
+            <?php endforeach; ?>
           </div>
-          <?php endforeach; ?>
+        </div>
+      <?php endforeach; ?>
     </div>
   </main>
-  <?php
-    include_once("templates/footer.php")
-  ?>
+<?php
+  include_once("templates/footer.php")
+?>
