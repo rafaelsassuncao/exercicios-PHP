1ecfcafb5ecf8d6380585081a146b653dae02726
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-01-31 06:26:26 -0300

    Nessa aula foi mostrado como editar a parte mobile no CSS

--- 15_blog/post.php
+++ 15_blog/post.php
@@ -2,32 +2,40 @@
   include_once("templates/header.php");
 
   if(isset($_GET['id'])) {
+
     $postId = $_GET['id'];
     $currentPost;
 
-    foreach($posts as $post){
+    foreach($posts as $post) {
       if($post['id'] == $postId) {
         $currentPost = $post;
       }
     }
+
   }
+
 ?>
   <main id="post-container">
     <div class="content-container">
-      <h1 id="main-title"><?=$currentPost['title'] ?></h1>
-      <p id="post-description"><?=$currentPost['description'] ?></p>
+      <h1 id="main-title"><?= $currentPost['title'] ?></h1>
+      <p id="post-description"><?= $currentPost['description'] ?></p>
       <div class="img-container">
-        <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?=$currentPost['title'] ?>">
+        <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
       </div>
-        <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime ipsa commodi sint veritatis? Tempora perferendis eligendi reprehenderit nihil aut? Reprehenderit nisi repellendus, error in corrupti itaque nihil explicabo eveniet!
-        Eligendi repellat repudiandae laboriosam autem aut asperiores tenetur repellendus unde facere quae beatae laborum dolor, ea odit vitae labore quisquam fuga perspiciatis cum, quidem blanditiis quos iure vel error. Dicta.
-        Nulla voluptatibus aspernatur id vitae fugiat. Quo facilis in saepe a eaque, consequatur fugit cum doloribus reiciendis rerum deserunt dignissimos cumque dolorum suscipit dolor vero nesciunt, unde dolores. Nisi, quidem.
-        Suscipit ipsum, quibusdam totam tempora vitae laboriosam reprehenderit maiores nobis vel dignissimos nostrum dolorem esse nihil error sapiente omnis! Rem ex eaque voluptatibus modi sequi inventore error ullam amet commodi!
-        Ad, quidem, exercitationem est unde laborum laboriosam a molestias error eaque dicta porro libero rerum amet tenetur deleniti enim sit facere corrupti voluptatibus illo modi? Tenetur iusto iure dignissimos sapiente!</p>
+      <p class="post-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo provident, voluptatum, veniam nihil repellat eveniet quae adipisci exercitationem quos minus corrupti placeat veritatis architecto excepturi laudantium nulla tenetur cupiditate.
+      Odit saepe voluptas voluptates, iusto minima dolore deleniti corporis itaque, rem facere inventore in sed cumque voluptatibus unde! Assumenda nam aspernatur eveniet id illo inventore ratione laboriosam iusto culpa provident.
+      Nam blanditiis autem fugiat officiis animi, adipisci consequuntur minima. Mollitia atque iste sapiente quod pariatur necessitatibus minus voluptatem rerum eos modi enim perspiciatis provident ducimus, iure nostrum nobis eum iusto.
+      Perspiciatis sed numquam animi quae assumenda nesciunt voluptatibus rem! Deserunt, quae ex, pariatur nemo eveniet ipsam delectus aperiam aut quas blanditiis repellat quibusdam debitis sequi odit nostrum? Dolores, iure quam!
+      Beatae vel corrupti laborum repudiandae, placeat neque officiis odit provident reiciendis ducimus amet rem. Exercitationem similique itaque labore asperiores quasi officia aspernatur quas soluta quos. Quasi aliquam in laboriosam illum.</p>
+      <p class="post-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, explicabo provident, voluptatum, veniam nihil repellat eveniet quae adipisci exercitationem quos minus corrupti placeat veritatis architecto excepturi laudantium nulla tenetur cupiditate.
+      Odit saepe voluptas voluptates, iusto minima dolore deleniti corporis itaque, rem facere inventore in sed cumque voluptatibus unde! Assumenda nam aspernatur eveniet id illo inventore ratione laboriosam iusto culpa provident.
+      Nam blanditiis autem fugiat officiis animi, adipisci consequuntur minima. Mollitia atque iste sapiente quod pariatur necessitatibus minus voluptatem rerum eos modi enim perspiciatis provident ducimus, iure nostrum nobis eum iusto.
+      Perspiciatis sed numquam animi quae assumenda nesciunt voluptatibus rem! Deserunt, quae ex, pariatur nemo eveniet ipsam delectus aperiam aut quas blanditiis repellat quibusdam debitis sequi odit nostrum? Dolores, iure quam!
+      Beatae vel corrupti laborum repudiandae, placeat neque officiis odit provident reiciendis ducimus amet rem. Exercitationem similique itaque labore asperiores quasi officia aspernatur quas soluta quos. Quasi aliquam in laboriosam illum.</p>
     </div>
     <aside id="nav-container">
       <h3 id="tags-title">Tags</h3>
-      <ul class="tags-list">
+      <ul id="tag-list">
         <?php foreach($currentPost['tags'] as $tag): ?>
           <li><a href="#"><?= $tag ?></a></li>
         <?php endforeach; ?>
@@ -38,8 +46,8 @@
           <li><a href="#"><?= $category ?></a></li>
         <?php endforeach; ?>
       </ul>
-    </aside> 
-  </main> 
+    </aside>
+  </main>
 <?php
-    include_once("templates/footer.php")
-  ?>
+  include_once("templates/footer.php")
+?>
