8555bb1bbad56518f754b43818419e232a26b7f5
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-02-13 19:00:22 -0300

    transformei os dados que estavam em mysqli em variáveis para assim ficar mais compreenssivel de se ler.

--- 16_php_mysql/01_mysqli.conexao/index.php
+++ 16_php_mysql/01_mysqli.conexao/index.php
@@ -1,3 +1,8 @@
 <?php
 
-  $conn = new mysqli("localhost", "root", "","cursophp");
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "cursophp";
+  
+  $conn = new mysqli($host, $user, $pass, $db);
