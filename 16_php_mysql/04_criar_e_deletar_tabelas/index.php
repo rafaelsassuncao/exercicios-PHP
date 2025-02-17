d64d117749883ad14ac7ca1a50da8531c219e0db
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-02-17 05:54:26 -0300

    Nessa aula o professor ensinou como criar e deletar tabelas do banco de dados por meio do php.

--- 16_php_mysql/04_criar_e_deletar_tabelas/index.php
+++ 16_php_mysql/04_criar_e_deletar_tabelas/index.php
@@ -8,3 +8,15 @@
  $conn = new mysqli($host, $user, $pass, $db);
 
 //  ASSUNTO DA AULA
+
+// Comando para criar uma tabela:
+// $q = "CREATE TABLE teste (nome VARCHAR(100), sobrenome VARCHAR(100))";
+
+// Comando para deletar (Dropar) uma tabela:
+ $q = "DROP TABLE teste";
+
+
+
+$conn->query($q);
+
+$conn->close();
