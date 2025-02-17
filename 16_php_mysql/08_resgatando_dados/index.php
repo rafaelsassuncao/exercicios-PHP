756921d3709e774d723b63b3fad04a42936b20c4
Rafael Assuncao <rafael_assuncao@outlook.com.br>
2025-02-17 14:06:02 -0300

    Aula sobre como recuperar dados por meio do php, concluida.

---
+++ 16_php_mysql/08_resgatando_dados/index.php
@@ -0,0 +1,19 @@
+<?php
+
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "cursophp";
+
+  // ASSUNTO DA AULA
+
+  $id = 4;
+  $stmt = $conn->prepare("SELECT * FROM itens WHERE id > ?");
+  $stmt->bind_param("i", $id);
+  $stmt->execute();
+
+  $result = $stmt->get_result();
+
+  $data = $result->fetch_all();
+
+  print_r($data);
