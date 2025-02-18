f165bc0fd329ab8eb3c68d4055686caa718b0fd2
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-02-18 06:03:46 -0300

    Conclui a aula sobre como resgatar dados.

--- 16_php_mysql/08_resgatando_dados/index.php
+++ 16_php_mysql/08_resgatando_dados/index.php
@@ -5,11 +5,15 @@
   $pass = "";
   $db = "cursophp";
 
-  // ASSUNTO DA AULA
+  $conn = new mysqli($host, $user, $pass, $db);
 
+  // ASSUNTO DA AULA
   $id = 4;
+
   $stmt = $conn->prepare("SELECT * FROM itens WHERE id > ?");
+
   $stmt->bind_param("i", $id);
+
   $stmt->execute();
 
   $result = $stmt->get_result();
