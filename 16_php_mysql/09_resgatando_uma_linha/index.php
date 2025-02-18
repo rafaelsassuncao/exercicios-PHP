99b9d4e36e0e83bbc0188098b93e6c0d11501c42
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-02-18 06:20:30 -0300

    Aula sobre como pegar somente uma linha especifica na tabela desejada no banco de dados concluida.

--- 16_php_mysql/09_resgatando_uma_linha/index.php
+++ 16_php_mysql/09_resgatando_uma_linha/index.php
@@ -8,8 +8,16 @@
   $conn = new mysqli($host, $user, $pass, $db);
 
   // ASSUNTO DA AULA
-  // Resgatando uma linha
+  // Resgatando uma linha somente!
 
-  $id = 6;
-  $stmt = $conn->prepare()
+  $id = 7;
+  $stmt = $conn->prepare("SELECT * FROM itens  WHERE id = ?");
+  $stmt->bind_param("i", $id); //Método certo
+  $stmt->execute();
+  
+  $result = $stmt->get_result();
+  $item = $result->fetch_row();//Método certo
 
+  print_r($item);
+
+  $conn->close(); // Fechar conexão
