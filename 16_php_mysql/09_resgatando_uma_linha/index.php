2a98a7f826c7866953fdac1f2bb71d05ae20ce95
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-02-18 06:09:01 -0300

    Nessa aula o professor esta ensinando como resgatar uma linha.

---
+++ 16_php_mysql/09_resgatando_uma_linha/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "cursophp";
+
+  $conn = new mysqli($host, $user, $pass, $db);
+
+  // ASSUNTO DA AULA
+  // Resgatando uma linha
+
+  $id = 6;
+  $stmt = $conn->prepare()
+
