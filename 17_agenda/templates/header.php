3965f880e7c4162681803ba07afa16db5af0c67f
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-02-26 06:36:31 -0300

    Nessa segunda aula o professor esta mostrando como é feita a separação em partes da nossa pagina.

---
+++ 17_agenda/templates/header.php
@@ -0,0 +1,22 @@
+<?php
+
+  include_once("config/url.php")
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agenda de Contatos</title>
+  <!-- BOOTSTRAP -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- FONT AWESOME -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- CSS -->
+   <link rel="stylesheet" href="<?=$BASE_URL?>css/style.css">
+</head>
+<body>
+  <header>
+    
+  </header> 
