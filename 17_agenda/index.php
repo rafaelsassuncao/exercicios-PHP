965623abdd8d6f340889c2d1e9fffb7317f15e80
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-05-28 21:34:21 -0300

    Exibe mensagem condicional e início da listagem de contatos

--- 17_agenda/index.php
+++ 17_agenda/index.php
@@ -1,8 +1,17 @@
 <?php
   include_once("templates/header.php");
 ?>
-  <h1>Testando Agenda</h1>
-  <i class="fas fa-eye"></i>
-  <?php
+  <div class="container">
+    <?php if(isset($printMsg) && $printMsg != ''): ?>
+        <p id="msg"><?= $printMsg ?></p>
+      <?php endif; ?>
+      <h1 id="main-title">Minha Agenda</h1>
+      <?php if(count($contacts > 0)): ?>
+        <p>Tem Contatos</p>
+      <?php else: ?>
+        <p id=""></p>
+
+  </div>
+<?php
   include_once("templates/footer.php");
 ?>
