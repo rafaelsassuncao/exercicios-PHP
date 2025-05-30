7e20efaaf2d9c03a50ef31ee1a43caa0cb36c40b
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-05-30 15:10:19 -0300

    Corrigido o cabeçalho da tabela de contatos na tela principal

--- 17_agenda/index.php
+++ 17_agenda/index.php
@@ -6,10 +6,19 @@
         <p id="msg"><?= $printMsg ?></p>
       <?php endif; ?>
       <h1 id="main-title">Minha Agenda</h1>
-      <?php if(count($contacts > 0)): ?>
-        <p>Tem Contatos</p>
+      <?php if(count($contacts) > 0): ?>
+        <table class="table" id="contacts-table"></table>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">#</th>
+              <th scope="col">#</th>
+              <th scope="col">#</th>
+            </tr>
+          </thead>
       <?php else: ?>
-        <p id=""></p>
+        <p id="empty-list-text">Ainda não  há contatos na sua agenda, <a href="<?=$BASE_URL ?>create.php">Clique para adicionar</a>.</p>
+      <?php endif; ?>
 
   </div>
 <?php
