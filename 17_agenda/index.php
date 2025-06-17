1ce954db24b5e1d5da0b527fddfa20a1ea5d5334
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-06-17 07:12:45 -0300

    Setup inicial: banco agenda e tabela contacts

--- 17_agenda/index.php
+++ 17_agenda/index.php
@@ -7,19 +7,26 @@
       <?php endif; ?>
       <h1 id="main-title">Minha Agenda</h1>
       <?php if(count($contacts) > 0): ?>
-        <table class="table" id="contacts-table"></table>
+       <table class="table" id="contacts-table>">
           <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">#</th>
-              <th scope="col">#</th>
-              <th scope="col">#</th>
-            </tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Telefone</th>
+            <th scope="col"></th>
           </thead>
+          <tbody>
+            <?php foreach($contacts as $contacts): ?>
+              <tr>
+                <td scope="row"><?= $contacts["id"] ?></td>
+                <td scope="row"><?= $contacts["name"] ?></td>
+                <td scope="row"><?= $contacts["phone"] ?></td>
+              </tr>
+            <?php endforeach; ?>  
+          </tbody>
+        </table>
       <?php else: ?>
         <p id="empty-list-text">Ainda não  há contatos na sua agenda, <a href="<?=$BASE_URL ?>create.php">Clique para adicionar</a>.</p>
       <?php endif; ?>
-
   </div>
 <?php
   include_once("templates/footer.php");
