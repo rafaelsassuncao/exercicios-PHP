705aeedf938cbbebef2ce92bd10494b6ab8528f0
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-06-23 07:11:06 -0300

    Feat: Ajuste no header e estilização do CSS/Index da listagem.

--- 17_agenda/index.php
+++ 17_agenda/index.php
@@ -17,13 +17,13 @@
           <tbody>
             <?php foreach($contacts as $contact): ?>
               <tr>
-                <td scope="row"><?= $contact["id"] ?></td>
+                <td scope="row" class="col-id"><?= $contact["id"] ?></td>
                 <td scope="row"><?= $contact["name"] ?></td>
                 <td scope="row"><?= $contact["phone"] ?></td>
                 <td class="action">
                   <a href="#"><i class="fas fa-eye check-icon"></i></a>
                   <a href="#"><i class="far fa-edit edit-icon"></i></a>
-                  <button type="summit"><i class="fas fa-times delete-icon"></i></button>
+                  <button type="summit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
                 </td>
               </tr>
             <?php endforeach; ?>  
--- 17_agenda/templates/header.php
+++ 17_agenda/templates/header.php
@@ -3,10 +3,10 @@
   include_once("config/process.php");
 
   // limpa a mensagem
-  if(isset($_SESSION['msg']))
-  $printMsg = $_SESSION['msg'];
-  $_SESSION['msg'] = '';
-  
+  if(isset($_SESSION['msg'])) { // Adicionado chaves para o if, boa prática
+    $printMsg = $_SESSION['msg'];
+    $_SESSION['msg'] = '';
+  } // Fechando o if
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -28,12 +28,12 @@
         <img src="<?=$BASE_URL?>img/logo.svg" alt="Agenda">
       </a>
       <div class="collapse navbar-collapse" id="navbarNav">
-          <div class="navbar-nav"> 
+          <div class="navbar-nav">
             <a class="nav-link active text-white" id="home-link" href="<?=$BASE_URL?>index.php">Agenda</a>
             <a class="nav-link active text-white" id="add-contact-link" href="<?=$BASE_URL?>create.php">Adicionar Contato</a>
           </div>
       </div>
-    </nav> 
+    </nav>
   </header>
 </body>
 </html>
