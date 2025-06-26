b36b9a77b09dd7e5187da924471678414f93b42b
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-06-26 06:33:26 -0300

    Adiciona telefone e observaçoes ao view do contato selecionado (show.php).

--- 17_agenda/show.php
+++ 17_agenda/show.php
@@ -3,6 +3,10 @@
 ?>
  <div class="container" id="view-contact-container">
     <h1 id="main-title"><?= $contact["name"] ?></h1>
+    <p class="bold">Telefone:</p>
+    <p><?= $contact["phone"] ?></p>
+    <p class="bold">Observações:</p>
+    <p><?= $contact["observation"] ?></p>
  </div>
 <?php
   include_once("templates/footer.php");
