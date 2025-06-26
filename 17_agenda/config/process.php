9ff6bf670eeeadbdec062064e428f23326ec503e
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-06-26 06:25:09 -0300

    Implementando visualização de contato único (show.php e ajustes na listagem (index.php e process.php) - estudos php.

--- 17_agenda/config/process.php
+++ 17_agenda/config/process.php
@@ -5,12 +5,30 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$contacts = [];
+$id = null;
+if(!empty($_GET)) {
+  $id = $_GET["id"];
+}
 
-$query = "SELECT * FROM contacts";
+// Retorna dado de um contato
+if(!empty($id)){
+  $query = "SELECT * FROM contacts WHERE id = :id";
+  $stmt = $conn->prepare($query);
+  $stmt->bindParam(":id", $id);
+  $stmt->execute();
+  $contact= $stmt->fetch();
 
-$stmt = $conn->prepare($query);
+}else{
+  // Retorna todos os contatos
+  $contacts = [];
+  
+  $query = "SELECT * FROM contacts";
+  
+  $stmt = $conn->prepare($query);
+  
+  $stmt->execute();
+  
+  $contacts = $stmt->fetchAll();
+}
 
-$stmt->execute();
 
-$contacts = $stmt->fetchAll();
---
+++ 17_agenda/show.php
@@ -0,0 +1,9 @@
+<?php
+  include_once("templates/header.php");
+?>
+ <div class="container" id="view-contact-container">
+    <h1 id="main-title"><?= $contact["name"] ?></h1>
+ </div>
+<?php
+  include_once("templates/footer.php");
+?>
