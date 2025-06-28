f9f39fd8559e56165ad8ea17e4c1cb20a2b51dc9
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-06-28 20:49:41 -0300

    Atualiza operações do banco e formulário de criação Modifica operações com o  banco em  process.php Altera atributo do formulario de ID para Value; Melhora tratamento de erros;

--- 17_agenda/config/process.php
+++ 17_agenda/config/process.php
@@ -5,30 +5,77 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$id = null;
-if(!empty($_GET)) {
-  $id = $_GET["id"];
-}
+$data = $_POST;
 
-// Retorna dado de um contato
-if(!empty($id)){
-  $query = "SELECT * FROM contacts WHERE id = :id";
-  $stmt = $conn->prepare($query);
-  $stmt->bindParam(":id", $id);
-  $stmt->execute();
-  $contact= $stmt->fetch();
+// MODIFICAÇÕES NO BANCO
+if(!empty($data)){
 
+  // Criar Contato
+  if($data["type"] === "create"){
+
+    $name         = $data["name"];
+    $phone        = $data["phone"];
+    $observation  = $data["observation"];
+
+    $query = "INSERT INTO contacts (name, phone, observation) VALUES (:name, :phone, :observation)";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":phone", $phone);
+    $stmt->bindParam(":observation", $observation);
+  
+    try {
+
+      $stmt->execute();
+      $_SESSION["msg"] = "Contato criado com sucesso";
+    
+    } catch(PDOException $e) {
+      // erro de conexão
+      $error = $e->getMessage();
+      echo "Erro: $error";
+    }
+
+  }
+
+  // Redirect HOME
+  header("location: " .$BASE_URL."../index.php");
+
+// SELEÇÃO DE DADOS
 }else{
-  // Retorna todos os contatos
-  $contacts = [];
-  
-  $query = "SELECT * FROM contacts";
-  
-  $stmt = $conn->prepare($query);
-  
-  $stmt->execute();
-  
-  $contacts = $stmt->fetchAll();
+
+  $id;
+
+  if(!empty($_GET)) {
+    $id = $_GET["id"];
+  }
+
+  // Retorna dado de um contato
+  if(!empty($id)){
+    $query = "SELECT * FROM contacts WHERE id = :id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+    $contact= $stmt->fetch();
+
+  }else{
+    // Retorna todos os contatos
+    $contacts = [];
+    
+    $query = "SELECT * FROM contacts";
+    
+    $stmt = $conn->prepare($query);
+    
+    $stmt->execute();
+    
+    $contacts = $stmt->fetchAll();
+  }
+
 }
+
+// FECHAR CONEXÃO
+$conn = null;
+
+
 
 
--- 17_agenda/create.php
+++ 17_agenda/create.php
@@ -5,7 +5,7 @@
     <?php include_once("templates/backbtn.html"); ?>
     <h1 id="main-title">Criar Contato</h1>
     <form id="create-form" action="<?=$BASE_URL ?>config/process.php" method="POST">
-      <input type="hidden" name="type" id="create">
+      <input type="hidden" name="type" value="create">
       <div class="form-group mb-4">
         <label for="name">Nome do Contato:</label>
         <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
