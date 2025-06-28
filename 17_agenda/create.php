ac0b5b9a174eb51560aa8d810c1af19e3bcca23d
Rafael_Assuncao <rafael_assuncao@outlook.com.br>
2025-06-28 18:45:27 -0300

    Ajusta layout do formulario create.php e centraliza o form via CSS

--- 17_agenda/create.php
+++ 17_agenda/create.php
@@ -1,7 +1,28 @@
 <?php
   include_once("templates/header.php");
 ?>
-  <h1>Criando Contato</h1>
-  <?php
+  <div class="container">
+    <?php include_once("templates/backbtn.html"); ?>
+    <h1 id="main-title">Criar Contato</h1>
+    <form id="create-form" action="<?=$BASE_URL ?>config/process.php" method="POST">
+      <input type="hidden" name="type" id="create">
+      <div class="form-group mb-4">
+        <label for="name">Nome do Contato:</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+      </div>  
+
+      <div class="form-group mb-4">
+        <label for="phone">Telefone de Contato:</label>
+        <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o nº de telefone" required>
+      </div>
+
+      <div class="form-group mb-4">
+        <label for="observation">Observações:</label>
+        <textarea type="text" class="form-control" id="observation" name="observation" placeholder="Digite uma observação" rows="3"></textarea>
+      </div>  
+      <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+  </div>
+<?php
   include_once("templates/footer.php");
 ?>
